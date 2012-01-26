846a50771a5923dcc63682c7c62582705dd3ac0e
Dominik Guzei <dominik_guzei@gmx.at>
2012-01-26 21:07:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlotCarMadness::Application.config.secret_token = 'b9ac4f22f4ebe469abcb9558554801ee70959e2949cd87299aa5bb537acccc49d83f84cd162fadedb174e5595303754cf838e11772af7fe3843fb79c01cf33fe'
